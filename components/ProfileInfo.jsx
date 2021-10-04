b9bb79110ced439872986982e89e2ac9716151c9
Naitsab43 <bastianhuenuqueo43@gmail.com>
2021-10-04 04:23:35 -0300

    Empece la creacion de la modal, faltan estilos y funcionalidad

--- components/ProfileInfo.jsx
+++ components/ProfileInfo.jsx
@@ -4,10 +4,13 @@ import styles from '../styles/profile.module.css'
 import buttonStyles from '../styles/buttons.module.css'
 
 import { AlertContext } from '../context/AlertContext'
+import { ModalContext } from '../context/ModalContext'
+import { Modal } from './Modal'
 
 export const ProfileInfo = ({user, title, image, _id, showButton=false }) => {
 
   const { alert, setAlert } = useContext(AlertContext)
+  const { showModal, setShowModal } = useContext(ModalContext)
 
 
   const copyToClipboard = e => {
@@ -30,52 +33,56 @@ export const ProfileInfo = ({user, title, image, _id, showButton=false }) => {
   }
 
   const editProfile = () => {
-
-
-
+    setShowModal(true)
   }
 
   return (
 
-    <div className={styles.profile}>
+    <>
 
-      <img className={styles.profileImage} src="https://play-lh.googleusercontent.com/IlnBc1ca_20U3qacgXrkXM_opQK9gvTXryaPSCCPCanD_o_hPdgPQkhQ6-DcsfSZ9PU9=s360" />
+      <Modal show={showModal} />
 
-      <div className={styles.profileInfoContainer}>
+      <div className={styles.profile}>
 
-        <div className={styles.infoGroup}>
-          <h2 className={styles.profileTitle}>{ title }</h2>
-          <span className={styles.profileUser}>{ user }</span>
+        <img className={styles.profileImage} src="https://play-lh.googleusercontent.com/IlnBc1ca_20U3qacgXrkXM_opQK9gvTXryaPSCCPCanD_o_hPdgPQkhQ6-DcsfSZ9PU9=s360" />
+
+        <div className={styles.profileInfoContainer}>
+
+          <div className={styles.infoGroup}>
+            <h2 className={styles.profileTitle}>{ title }</h2>
+            <span className={styles.profileUser}>{ user }</span>
+          </div>
+          
+          {
+
+            showButton && 
+
+            <>
+
+              <button 
+                className={`${buttonStyles.form__button} ${buttonStyles["form__button--copy"]}`}
+                onClick={copyToClipboard}
+              >
+                Copiar ID
+              </button>
+
+              
+              <button 
+                className={`${buttonStyles.form__button} ${buttonStyles["form__button--edit"]}`}
+                onClick={editProfile}
+              >
+                Editar perfil
+              </button>
+
+            </>
+
+          }
+
         </div>
-        
-        {
-
-          showButton && 
-
-          <>
-
-            <button 
-              className={`${buttonStyles.form__button} ${buttonStyles["form__button--copy"]}`}
-              onClick={copyToClipboard}
-            >
-              Copiar ID
-            </button>
-
-            
-            <button 
-              className={`${buttonStyles.form__button} ${buttonStyles["form__button--edit"]}`}
-              onClick={editProfile}
-            >
-              Editar perfil
-            </button>
-
-          </>
-
-        }
 
       </div>
-      
-    </div>
+
+    </>
 
   )
 
